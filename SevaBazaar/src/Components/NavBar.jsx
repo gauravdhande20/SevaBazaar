2a70bffde8854722c159587fa2c19f230e697d28
gauravdhande20 <gauravdhande2022004@gamil.com>
2025-12-21 13:30:58 +0530

    done

--- SevaBazaar/src/Components/NavBar.jsx
+++ SevaBazaar/src/Components/NavBar.jsx
@@ -5,6 +5,17 @@ function NavBar() {
     <div>
       <section className="bg-blue-500 text-white p-4">
         <h1 className="text-xl font-bold">NavBar</h1>
+        <nav className="flex space-x-4">
+          <a href="#" className="hover:underline">Home</a>
+          <a href="#" className="hover:underline">Products</a>
+          <a href="#" className="hover:underline">Services</a>
+          <a href="#" className="hover:underline">Contact</a>
+        </nav>
+        <div className="icons">
+          <span className="material-icons mr-4 cursor-pointer">search</span>
+          <span className="material-icons cursor-pointer">shopping_cart</span>
+        </div>
+
       </section>
     </div>
   )

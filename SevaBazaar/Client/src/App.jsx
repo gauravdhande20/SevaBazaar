906a713e66d02b57fad634d06ba81f4006dad266
gauravdhande20 <gauravdhande2022004@gmail.com>
2025-12-27 09:25:11 +0530

    card Displya

--- SevaBazaar/Client/src/App.jsx
+++ SevaBazaar/Client/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import NavBar from './Components/NavBar.jsx'
 import HeroSec from './Components/HeroSec.jsx' 
+import BasicCard from './Components/BasicCard.jsx'
 import Cards from './Components/Cards.jsx' 
 import Footer from './Components/Footer.jsx'
-import itemdata from './../public/items.json'
+
 
 
 function App() {
@@ -11,25 +12,9 @@ function App() {
     <>
       <NavBar/>
       <HeroSec/>
-      {
-    itemdata.map(item =>{
-      return(
-    <Cards 
-      id={item.id}
-      url={item.url}
-      color={item.color}
-      offer={item.offer}
-      off={item.off}
-      Catg={item.Catg}
-      Name={item.Name}
-      quantity={item.quantity}
-      price={item.price}
-      prv_price={item.prv_price}
-      />)})
-      }
+     <BasicCard/>
       <Footer/>
-      
-    </>
+        </>
   )
 }
 
---
+++ SevaBazaar/Client/src/Components/BasicCard.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Cards from './Cards'
+
+import itemdata from './../../public/items.json'
+
+function BasicCard() {
+    return (
+        <>
+            <section className='h-auto w-full bg-amber-100 '>
+                <div>
+                    <h1>Offer List</h1>
+                    <p>Lorem ipsum dolor sit amet consectenisi error minima explicabo ex.</p>
+                </div>
+                <div className='grid grid-cols-4 gap-4 mt-10 mb-10 ml-10 mr-10'>
+                     {
+    itemdata.map(item =>{
+      return(
+    <Cards 
+      id={item.id}
+      url={item.url}
+      color={item.color}
+      offer={item.offer}
+      off={item.off}
+      Catg={item.Catg}
+      Name={item.Name}
+      quantity={item.quantity}
+      price={item.price}
+      prv_price={item.prv_price}
+      />)})
+      }
+
+                </div>
+
+
+
+            </section>
+
+        </>
+    )
+}
+
+export default BasicCard
